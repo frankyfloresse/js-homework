9f8fc603e690132ede14e0e3e9c239ed7c0535a6
frankyfloresse <foz.fox2015@gmail.com>
2024-11-04 16:43:32 +0200

    complete ruUtWDUI task

---
+++ lesson1/ruUtWDUI/main.js
@@ -0,0 +1,6 @@
+let firstName = prompt('Введіть ваше імʼя:');
+let middleName = prompt('Введіть ваше імʼя по-батькові:');
+let age = prompt('Введіть ваш вік:');
+
+console.log(`${firstName} ${middleName}, ${age} роки`);
+
