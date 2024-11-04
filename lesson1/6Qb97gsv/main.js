7461ed5fcb342faaffbc5ea74ce6add4ec086692
frankyfloresse <foz.fox2015@gmail.com>
2024-11-04 15:53:18 +0200

    complete 6Qb97gsv task

---
+++ lesson1/6Qb97gsv/main.js
@@ -0,0 +1,9 @@
+let firstName = 'Тетяна';
+let middleName = 'Олександрівна';
+let lastName = 'Жорник';
+
+let person = lastName + ' ' + firstName + ' ' + middleName;
+console.log(person);
+
+let person2 = `${lastName} ${firstName} ${middleName}`;
+console.log(person2);
