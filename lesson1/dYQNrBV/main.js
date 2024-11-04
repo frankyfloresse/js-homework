351cd15253f6c966131622df4e24f4fe3a36b7c7
frankyfloresse <foz.fox2015@gmail.com>
2024-11-04 16:02:43 +0200

    update dYQNrBV task

--- lesson1/dYQNrBV/main.js
+++ lesson1/dYQNrBV/main.js
@@ -1,40 +1,30 @@
 let greeting = 'hello';
-console.log(greeting);
-
 let university = 'owu';
-console.log(university);
-
 let domain = 'com';
-console.log(domain);
-
 let country = 'ua';
-console.log(country);
-
 let one = 1;
-console.log(one);
-
 let ten = 10;
-console.log(ten);
-
 let negativeInteger = -999;
-console.log(negativeInteger);
-
 let positiveInteger = 123;
-console.log(positiveInteger);
-
 let pi = 3.14;
-console.log(pi);
-
 let float = 2.7;
-console.log(float);
-
 let age = 16;
-console.log(age);
-
 let isTrue = true;
-console.log(isTrue);
-
 let isFalse = false;
+
+
+console.log(greeting);
+console.log(university);
+console.log(domain);
+console.log(country);
+console.log(one);
+console.log(ten);
+console.log(negativeInteger);
+console.log(positiveInteger);
+console.log(pi);
+console.log(float);
+console.log(age);
+console.log(isTrue);
 console.log(isFalse);
 
 
