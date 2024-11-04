3c95713748ea8b8aa08306276ebf43d222bd8b3e
frankyfloresse <foz.fox2015@gmail.com>
2024-11-04 16:03:16 +0200

    complete 4N0y5tufA task

---
+++ lesson1/4N0y5tufA/main.js
@@ -0,0 +1,9 @@
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a); //number
+console.log(typeof b); //string
+console.log(typeof c); //boolean
