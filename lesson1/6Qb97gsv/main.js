14312552866f746f2e9ab464817a8cdc2880f3fa
frankyfloresse <foz.fox2015@gmail.com>
2024-11-04 16:49:16 +0200

    add tasks: dYQNrBv, 6Qb97gsv, 4N0y5tufA, ruUtWDUI

--- lesson1/6Qb97gsv/main.js
+++ lesson1/6Qb97gsv/main.js
@@ -1,3 +1,7 @@
+// 6Qb97gsv
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ
+// З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
 let firstName = 'Тетяна';
 let middleName = 'Олександрівна';
 let lastName = 'Жорник';
--- lesson1/dYQNrBV/main.js
+++ lesson1/dYQNrBV/main.js
@@ -1,3 +1,7 @@
+// dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false
+// Вивести кожну змінну за допомогою: console.log
+
 let greeting = 'hello';
 let university = 'owu';
 let domain = 'com';
